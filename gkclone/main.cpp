4c04250a80a22540b586c713a15b2dda09a7cd85
Danny <dannyvanswieten@gmail.com>
2018-03-02 15:08:01 +0100

    Initial Commit

---
+++ gkclone/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gkclone
+//
+//  Created by Danny on 02/03/2018.
+//  Copyright © 2018 Danny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
